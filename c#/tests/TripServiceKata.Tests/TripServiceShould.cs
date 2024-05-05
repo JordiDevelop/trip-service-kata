db2ca594fab457627c5291656b57fa8dbc621bcd
JordiDevelop <jordihdezhdez@gmail.com>
2024-05-05 10:10:37 +0100

    test fallido por implementación

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,41 @@
-﻿namespace TripServiceKata.Tests
+﻿using System.Collections.Generic;
+using TripServiceKata.Entity;
+using TripServiceKata.Service;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        [Fact]
+        public void GetTripsByUser()
+        {
+            var userSessionMock = new UserSessionMock();
+            var tripDaoWrapper = new TripDaoWrapperMock();
+            var tripService = new TripService(userSessionMock, tripDaoWrapper);
+            var userMock = new User();
+            Assert.Empty(tripService.GetTripsByUser(userMock));
+        }
+    }
+
+    public class TripDaoWrapperMock : ITripDaoWrapper
+    {
+        public List<Trip> FindTripsByUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class UserSessionMock : IUserSession
+    {
+        public bool IsUserLoggedIn(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetLoggedUser()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
