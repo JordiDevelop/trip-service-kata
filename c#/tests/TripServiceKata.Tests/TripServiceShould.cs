7fb4d347311463a053be8a682c5bc0fb4f06bb12
JordiDevelop <jordihdezhdez@gmail.com>
2024-05-05 16:07:30 +0100

    test de caracterización correctos

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
 using TripServiceKata.Entity;
+using TripServiceKata.Exception;
 using TripServiceKata.Service;
 using Xunit;
 
@@ -7,35 +10,81 @@ namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+        private class UserSessionMock : IUserSession
+        {
+            private readonly User _loggedUser;
+
+            public UserSessionMock(User loggedUser)
+            {
+                _loggedUser = loggedUser;
+            }
+
+            public bool IsUserLoggedIn(User user)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public User GetLoggedUser()
+            {
+                return _loggedUser;
+            }
+        }
+
+        private class TripDaoMock : ITripDaoWrapper
+        {
+            private readonly Dictionary<User, List<Trip>> _userTripMap = new Dictionary<User, List<Trip>>();
+
+            public void AddTripsForUser(User user, List<Trip> trips)
+            {
+                _userTripMap[user] = trips;
+            }
+
+            public List<Trip> FindTripsByUser(User user)
+            {
+                return _userTripMap.ContainsKey(user) ? _userTripMap[user] : new List<Trip>();
+            }
+        }
+
         [Fact]
-        public void GetTripsByUser()
+        public void GetTripsByUser_UserNotLoggedIn_ThrowsException()
         {
-            var userSessionMock = new UserSessionMock();
-            var tripDaoWrapper = new TripDaoWrapperMock();
-            var tripService = new TripService(userSessionMock, tripDaoWrapper);
-            var userMock = new User();
-            Assert.Empty(tripService.GetTripsByUser(userMock));
-        }
-    }
+            // Arrange
+            var userSession = new UserSessionMock(null);
+            var tripDaoWrapper = new TripDaoMock();
+            var tripService = new TripService(userSession, tripDaoWrapper);
 
-    public class TripDaoWrapperMock : ITripDaoWrapper
-    {
-        public List<Trip> FindTripsByUser(User user)
-        {
-            throw new System.NotImplementedException();
-        }
-    }
-
-    public class UserSessionMock : IUserSession
-    {
-        public bool IsUserLoggedIn(User user)
-        {
-            throw new System.NotImplementedException();
+            Assert.Throws<UserNotLoggedInException>(() => tripService.GetTripsByUser(new User()));
         }
 
-        public User GetLoggedUser()
+        [Fact]
+        public void GetTripsByUser_UserLoggedInButNotFriend_ReturnsEmptyList()
         {
-            throw new System.NotImplementedException();
+            // Arrange
+            var loggedUser = new User();
+            var userSession = new UserSessionMock(loggedUser);
+            var user = new User();
+            var tripDaoWrapper = new TripDaoMock();
+            var tripService = new TripService(userSession, tripDaoWrapper);
+            
+            var result = tripService.GetTripsByUser(user);
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetTripsByUser_UserLoggedInAndIsFriend_ReturnsTripList()
+        {
+            // Arrange
+            var loggedUser = new User();
+            var userSession = new UserSessionMock(loggedUser);
+            var user = new User();
+            user.AddFriend(loggedUser);
+            var trips = new List<Trip>() { new Trip(), new Trip() };
+            var tripDaoWrapper = new TripDaoMock();
+            tripDaoWrapper.AddTripsForUser(user, trips);
+            var tripService = new TripService(userSession, tripDaoWrapper);
+            
+            var result = tripService.GetTripsByUser(user);
+            Assert.Equal(trips, result);
         }
     }
 }
